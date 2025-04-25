545c5fff09c55600524da382f5be950ecad9e303
Ian-Cardona <iancardona.dev@gmail.com>
2025-04-25 23:24:26 +0800

    Part4: Added exercise 4.15

--- part4/blog_list/controllers/users.js
+++ part4/blog_list/controllers/users.js
@@ -1,14 +1,31 @@
 const usersRouter = require("express").Router();
 const User = require("../models/user");
+const bcrypt = require("bcrypt");
 
-usersRouter.post("/api/users", async (request, response, next) => {
-  const user = new User(request.body);
+usersRouter.get("/", async (_, response, next) => {
   try {
-    const savedUser = await user.save();
-    response.status(201).json(savedUser);
+    const users = await User.find({});
+    response.json(users);
   } catch (error) {
     next(error);
   }
+});
+
+usersRouter.post("/", async (request, response, _) => {
+  const { username, name, password } = request.body;
+
+  const saltRounds = 10;
+  const passwordHash = await bcrypt.hash(password, saltRounds);
+
+  const user = new User({
+    username,
+    name,
+    passwordHash,
+  });
+
+  const savedUser = await user.save();
+
+  response.status(201).json(savedUser);
 });
 
 module.exports = usersRouter;
