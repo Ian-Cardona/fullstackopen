a535e702a04a55f1d44c8c06942eadbd5f9f3d0f
Ian-Cardona <iancardona.dev@gmail.com>
2025-05-27 22:29:10 +0800

    Part9: Added exercise 9.6 and 9.7

--- part9/bmi/src/index.ts
+++ part9/bmi/src/index.ts
@@ -1,8 +1,10 @@
 import express from 'express';
 import qs from 'qs';
 import { calculateBmi } from '../bmiCalculator';
+import { calculateExercises } from '../exerciseCalculator';
 
 const app = express();
+app.use(express.json());
 
 app.set('query parser', (str: string) => qs.parse(str, { parameterLimit: 2 }));
 
@@ -22,7 +24,49 @@ app.get('/bmi', (req, res) => {
     res.send(bmi);
   } catch (error) {
     if (error instanceof Error) {
-      res.send({
+      res.status(400).send({
+        error: error.message,
+      });
+    }
+  }
+});
+
+app.post('/exercises', (req, res) => {
+  console.log('Request', req.body);
+
+  try {
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-member-access
+    const dailyExercises = req.body.daily_exercises;
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-member-access
+    const target = req.body.target;
+
+    if (!dailyExercises || !target) {
+      throw new Error('parameters missing');
+    }
+
+    if (!Array.isArray(dailyExercises)) {
+      throw new Error('invalid array');
+    }
+
+    const dailyExerciseNumbers = dailyExercises.map((d: unknown) => {
+      const num = Number(d);
+      if (isNaN(num)) {
+        throw new Error('invalid array');
+      }
+      return num;
+    });
+
+    if (isNaN(Number(target))) {
+      throw new Error('malformatted parameters');
+    }
+
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-call
+    const exercises = calculateExercises(Number(target), dailyExerciseNumbers);
+
+    res.send(exercises);
+  } catch (error) {
+    if (error instanceof Error) {
+      res.status(400).send({
         error: error.message,
       });
     }
