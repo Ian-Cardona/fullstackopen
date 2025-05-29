9490d21b02c91a837d31b336fc368dd77c7e7c93
Ian-Cardona <iancardona.dev@gmail.com>
2025-05-29 19:58:13 +0800

    Part9: Added exercise 9.10 update

--- part9/patientor_backend/src/index.ts
+++ part9/patientor_backend/src/index.ts
@@ -1,22 +1,19 @@
 import express from 'express';
 import cors from 'cors';
+import diagnosesRouter from './controllers/diagnoses';
 
 const app = express();
 
-// const corsOptions = {
-//     origin: ['http://localhost:3003', 'http://localhost:5173'],
-//     methods: ['GET', 'POST', 'PUT', 'DELETE'],
-//     allowedHeaders: ['Content-Type', 'Authorization'],
-// };
-
 app.use(cors());
 app.use(express.json());
+const PORT = 3003;
 
 app.get('/api/ping', (_req, res) => {
     res.send('pong');
 });
 
-const PORT = 3003;
+app.use('/api/diagnoses', diagnosesRouter);
+
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
