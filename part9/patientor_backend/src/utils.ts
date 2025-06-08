0ddde06f39a15980adffd9dcb5ed302f2429f705
Ian-Cardona <iancardona.dev@gmail.com>
2025-06-08 20:55:27 +0800

    Part9: Added exercise 9.21

--- part9/patientor_backend/src/utils.ts
+++ part9/patientor_backend/src/utils.ts
@@ -1,17 +1,22 @@
 import {z} from 'zod';
 import { Gender } from './types';
 
+export const entrySchema = z.object({
+    
+});
+
 export const newPatientSchema = z.object({
      name: z.string(),
      dateOfBirth: z.string(),
      ssn: z.string(),
      gender: z.nativeEnum(Gender),
-     occupation: z.string()
+     occupation: z.string(),
+     entries: z.array(entrySchema)
     },
 );
 
 export const patientEntrySchema = newPatientSchema.extend({
     id: z.string()
-});
+},);
 
-export const nonSensitivePatientEntrySchema = patientEntrySchema.omit({ ssn: true });
+export const nonSensitivePatientEntrySchema = patientEntrySchema.omit({ ssn: true, entries: true });
