ee9fbda682629d61cd3f46d493d3bb404a817ab8
Ian Cardona <iancardona.dev@gmail.com>
2025-07-23 21:46:36 +0800

    Part12: Added exercise 12.13

--- part12/todo-app/todo-frontend/src/Todos/List.jsx
+++ part12/todo-app/todo-frontend/src/Todos/List.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react"
+import Todo from "./Todo";
 
 const TodoList = ({ todos, deleteTodo, completeTodo, updateTodo }) => {
   const [editingTodoId, setEditingTodoId] = useState(null);
@@ -31,63 +32,20 @@ const TodoList = ({ todos, deleteTodo, completeTodo, updateTodo }) => {
 
   return (
     <>
-      {todos.map(todo => {
-        const isEditingThisTodo = editingTodoId === todo._id;
-
-        const doneInfo = (
-          <>
-            <span>This todo is done</span>
-            <span>
-              <button onClick={onClickEdit(todo)}> Edit </button>
-              {
-                !isEditingThisTodo ? <button onClick={onClickDelete(todo)}> Delete </button> : null
-              }
-            </span>
-          </>
-        )
-
-        const notDoneInfo = (
-          <>
-            <span>
-              This todo is not done
-            </span>
-            <span>
-              <button onClick={onClickEdit(todo)}> Edit </button>
-              {
-                !isEditingThisTodo ? <button onClick={onClickDelete(todo)}> Delete </button> : null
-              }
-              <button onClick={onClickComplete(todo)}> Set as done </button>
-            </span>
-          </>
-        )
-
-        return (
-          <div style={{ display: 'flex', justifyContent: 'space-between', maxWidth: '70%', margin: 'auto' }} key={todo._id}>
-            {
-              isEditingThisTodo ? (
-                <form onSubmit={onSubmitEdit(todo)} style={{ display: 'flex', alignItems: 'center', width: '100%' }}>
-                  <input type="text" value={todo.text} disabled style={{ marginRight: '0.5rem' }} />
-                  <input
-                    type="text"
-                    placeholder="Edit notes"
-                    value={editTodo}
-                    onChange={e => setEditTodo(e.target.value)}
-                    style={{ marginRight: '0.5rem' }}
-                  />
-                  <button type="submit">Save</button>
-                  <button type="button" onClick={onCancelEdit}>Cancel</button>
-                </form>
-              ) : (
-                <span>
-                  {todo.text}
-                  {todo.notes ? <span style={{ fontStyle: 'italic', color: '#888', marginLeft: '0.5rem' }}>{`Notes: ${todo.notes}`}</span> : null}
-                </span>
-              )
-            }
-            {!isEditingThisTodo && (todo.done ? doneInfo : notDoneInfo)}
-          </div>
-        )
-      }).reduce((acc, cur, idx) => {
+      {todos.map((todo) => (
+        <Todo
+          key={todo._id}
+          todo={todo}
+          editingTodoId={editingTodoId}
+          editTodo={editTodo}
+          setEditTodo={setEditTodo}
+          onClickDelete={onClickDelete}
+          onClickComplete={onClickComplete}
+          onClickEdit={onClickEdit}
+          onSubmitEdit={onSubmitEdit}
+          onCancelEdit={onCancelEdit}
+        />
+      )).reduce((acc, cur, idx) => {
         if (idx === 0) return [cur];
         return [...acc, <hr key={`hr-${idx}`} />, cur];
       }, [])}
